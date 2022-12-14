b728be7d7e5c6a2d0d12e0d2b8c9e38717cbb719
camilo <camilo@MacBook-Air-de-camilo.local>
2022-12-14 14:06:44 -0300

    production

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@
 //export const URL = 'http://localhost:8000';
 
 //produccion
-export const URL = 'https://137.184.176.199:8000';
+export const URL = 'http://137.184.176.199:8000';
+
 
 //# de items a mostrar en la paginacion 
 export const ITEMS = 10
