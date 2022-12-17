1e99f3e870cf320bace4a43aa6bd1c2276e1d971
camilo <camilo@MacBook-Air-de-camilo.local>
2022-12-17 19:04:29 -0300

    se modifico el navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext,useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { Collapse, DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, NavbarText, NavbarToggler, NavItem, UncontrolledDropdown } from 'reactstrap';
 import { AuthContext } from '../context/AuthContext';
 
 import Caja from './Caja';
@@ -11,79 +12,80 @@ const Header = () => {
     logoutUser
   } = useContext(AuthContext)
   
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   
   
   return (
-    
-      <nav className="navbar fixed-top navbar-expand-lg bg-light p-2">
-        {
-        user? 
-        <>
-        <span className="navbar-brand p-2"><Caja /></span>
+    <div>
+      {
+        user?
         
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"  aria-expanded="false">
+      <Navbar className='fixed-top navbar-expand-lg bg-light'>
+        <NavbarBrand href="/"><span className="navbar-brand p-2"><Caja /></span></NavbarBrand>
+        <NavbarToggler onClick={toggle}  />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <Link className="nav-link" aria-current="page" to="/" onClick={toggle} >Home</Link>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
                 Informes
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/recaudos/">Recaudos</Link></li>
-                <li><Link className="dropdown-item" to="/cierres/">Cierres Caja</Link></li>
-              </ul>              
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Ingresos-Egresos
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/aportes/">Aportes</Link></li>
-                <li><Link className="dropdown-item" to="/gastos/">Gastos</Link></li>
-                <li><Link className="dropdown-item" to="/utilidades/">Utilidades</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/ventas/">Ventas</Link>
-            </li>            
-            <li className="nav-item">
-              <Link className="nav-link" to="/liquidar/">Liquidar</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem><Link className="dropdown-item" to="/recaudos/" onClick={toggle}>Recaudos</Link></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><Link className="dropdown-item" to="/cierres/" onClick={toggle}>Cierres Caja</Link></DropdownItem>                
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Aportes-Gastos
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem><Link className="dropdown-item" to="/aportes/" onClick={toggle}>Aportes</Link></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><Link className="dropdown-item" to="/gastos/" onClick={toggle}>Gastos</Link></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><Link className="dropdown-item" to="/utilidades/" onClick={toggle}>Utilidades</Link></DropdownItem>                
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <Link className="nav-link" to="/ventas/" onClick={toggle}>Ventas</Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="/liquidar/" onClick={toggle}>Liquidar</Link>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
                 Usuarios
-              </a>
-              <ul className="dropdown-menu">
-                <li><Link className="dropdown-item" to="/clientes/">Clientes</Link></li>
-                <li><Link className="dropdown-item" to="/trabajadores/">Trabajadores</Link></li>
-              </ul>
-            </li>           
-
-            
-          </ul>
-          {user &&
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                
-                <li className="nav-item">
-                  <Link className="nav-link" to={`/trabajadores/${perfil.id}/`}>Usuario: {user.first_name}</Link>
-                </li>
-                <li className="nav-item">
-                  <button className="nav-link btn btn-outline-light" to="/login/" onClick={logoutUser}>Salir</button>
-                </li>
-            </ul>        
-          } 
-        </div>
-        
-        </>:null
-        }
-        </nav>
-    
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem><Link className="dropdown-item" to="/clientes/" onClick={toggle}>Clientes</Link></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><Link className="dropdown-item" to="/trabajadores/" onClick={toggle}>Trabajadores</Link></DropdownItem>                                            
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            {user && <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <NavItem>
+                        <Link className="nav-link" to={`/trabajadores/${perfil.id}/`} onClick={toggle}>Usuario: {user.first_name}</Link>
+                      </NavItem>
+                      <NavItem>
+                        <button className="nav-link btn btn-outline-light" to="/login/" onClick={logoutUser}>Salir</button>
+                      </NavItem> 
+                    </ul>              
+            } 
+          
+          </Nav>
+          
+        </Collapse>
+      </Navbar>
+      :
+      null
+      }
+    </div>              
     
   )
 
