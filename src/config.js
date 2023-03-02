780c37985669b4d1eea67dae2359e187a6ea7c31
cavb1205 <cavb.1205@hotmail.com>
2023-03-02 13:46:55 -0300

    prodct

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 ///desarrollo
-export const URL = 'http://192.168.1.160:8000';
+// export const URL = 'http://192.168.1.160:8000';
 //export const URL = 'http://localhost:8000';
 
 //produccion
-// export const URL = 'http://137.184.176.199:8000';
+ export const URL = 'http://137.184.176.199:8000';
 
 
 //# de items a mostrar en la paginacion 
