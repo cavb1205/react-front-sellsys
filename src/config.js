668c6ec854b064d3fc691d094b9c30d099aff895
cavb1205 <cavb.1205@hotmail.com>
2023-09-06 10:56:41 -0300

    prod

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 ///desarrollo
 // export const URL = 'http://192.168.1.83:8000';
-export const URL = 'http://localhost:8000';
+// export const URL = 'http://localhost:8000';
 
 //produccion
-//  export const URL = 'https://api.carterafinanciera.com';
+ export const URL = 'https://api.carterafinanciera.com';
 
 
 //# de items a mostrar en la paginacion 
